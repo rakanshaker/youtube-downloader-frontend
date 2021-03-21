351f68f971b1f75f8d2be68b0b0ae7da1e4945b3
rakanshaker <shaker.rakan@gmail.com>
2021-03-21 16:36:28 -0400

    enable download from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import youtubeDl from "./api-consumers/youtube-downloader";
+import "./App.css";
 
 function App() {
+  const [youtubeLink, setYoutubeLink] = useState("");
+  const [formats, setFormats] = useState([]);
+  const [selectedFormatId, setSelectedFormatId] = useState("");
+
+  const handleSelect = (ev) => {
+    const formatId = ev.target.id;
+
+    setSelectedFormatId(formatId);
+  };
+
+  const renderFormats = formats.map((item, index) => {
+    return (
+      <div key={index}>
+        <input
+          type="radio"
+          name="format"
+          onClick={handleSelect}
+          id={item.format_id}
+        />
+        <label>{`${item.ext} at resolution ${item.format_note}`}</label>
+      </div>
+    );
+  });
+
+  const handleSubmit = async (ev) => {
+    ev.preventDefault();
+    const vidFormats = await youtubeDl.getFormats(youtubeLink).catch((err) => {
+      console.log("could not fetch formats due to =>", err);
+      return [];
+    });
+    console.log(vidFormats);
+    setFormats(vidFormats);
+  };
+
+  const handleInput = (ev) => {
+    setYoutubeLink(ev.target.value);
+  };
+
+  const handleDownloadClick = async () => {
+    try {
+      const download = await youtubeDl.downloadVideo(
+        youtubeLink,
+        selectedFormatId
+      );
+      window.open(download.url);
+    } catch (err) {
+      console.log("could not download =>", err);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Youtube Downloader</h2>
+      <p>Download Youtube Videos for Free</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleInput} />
+        <input type="submit" />
+      </form>
+
+      <div className="formats">{renderFormats}</div>
+      <button onClick={handleDownloadClick}>Download</button>
     </div>
   );
 }
---
+++ src/utils/url.js
@@ -0,0 +1,20 @@
+export const getAllQueryParameter = (link = "") => {
+  let query_params = {};
+  if (link.indexOf("?") >= 0) {
+    link
+      .split("?")[1]
+      .split("&")
+      .map((el) => {
+        let x = {};
+        let key = el.split("=")[0];
+        let value = el.split("=")[1];
+        x[key] = value;
+        return x;
+      })
+      .forEach((el) => {
+        query_params = { ...query_params, ...el };
+      });
+    return query_params;
+  }
+  return {};
+};
