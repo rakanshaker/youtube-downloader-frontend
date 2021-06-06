8149f6fc4b8d1080c1e9a31f2c99c18bc10ef960
ahmad baderkhan <ahmadbaderkhan97@gmail.com>
2021-06-05 21:41:25 -0400

    fix w modal

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -13,11 +13,17 @@ import { CircularProgress, Modal } from '@material-ui/core';
 import { formattedNum } from '../utils/FormattedNum';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import FileSaver from 'file-saver';
+
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogActions from '@material-ui/core/DialogActions';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
 import DeviceDetector from "device-detector-js";
 
 const useStyles = makeStyles((theme) => ({
@@ -49,10 +55,28 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         justifyContent: 'center'
 
-    }
+    },
+    closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500],
+    },
+
 }));
 
+const DialogTitle = (({ children, classes, onClose }) => {
+    return (
+        <MuiDialogTitle disableTypography className={classes.root}>
+            <Typography variant="h6">{children}</Typography>
 
+            <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+                <CloseIcon />
+            </IconButton>
+
+        </MuiDialogTitle>
+    );
+});
 
 
 export default function MediaCard({
@@ -144,17 +168,23 @@ export default function MediaCard({
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description"
             >
-                <DialogTitle id="alert-dialog-title">Download Link Generated</DialogTitle>
+                <DialogTitle id="alert-dialog-title" classes={classes} onClose={() => setDownloadLink({})}>Download Link Generated</DialogTitle>
                 <DialogContent>
                     <DialogContentText id="alert-dialog-description">
-                        If you're on iPhone (safari) hold the link and choose the option Download Linked File. 
+                        If you're on iPhone (safari) hold the link and choose the option Download Linked File.
                         <br></br>
                         <br></br>
                         Note this is not a long term link , it will expire in 15 minutes.
           </DialogContentText>
                 </DialogContent>
                 <DialogActions className={classes.actionsBar}>
-                    <a href={downloadResponse.url} ><Typography>Download</Typography></a>
+                    <a download={downloadResponse.url ? downloadResponse.url.split('?')[0].split('/').pop() : ''} href={downloadResponse.url} onTouchDown={(ev) => {
+                        const link = ev.target.parentElement;
+
+                        ev.preventDefault();
+                        console.log('calling fileSaver',link.download);
+                        FileSaver.saveAs(link.href, link)
+                    }} ><Typography>Download</Typography></a>
                 </DialogActions>
             </Dialog>
 
